07dbb0607f40d38a1bde3397da172df2047a71f2
omniasameh92 <omniaawahab92@gmail.com>
2016-06-04 15:30:36 +0200

    orderupdate+ delivery man  models

--- DeliveryMan/delivery.py
+++ DeliveryMan/delivery.py
@@ -10,3 +10,19 @@ class MobileNumbers(models.Model):
         _name = 'project.mobilenumbers'
         mobile_nu = fields.Char(string='Mobile numbers')
         partner_id=fields.Many2one('res.partner')
+
+class OrderUpdate(models.Model):
+        _inherit='pos.order'
+        delivery_man_id=fields.Many2one('project.delivery')
+
+class delivery(models.Model):
+      _name='project.delivery'
+      name = fields.Char()
+      email=fields.Char()
+      image = fields.Binary()
+      date_of_birth = fields.Date()
+      phone_ids =  fields.One2many('project.phonenumbers', 'partner_id', string='phone numbers')
+      mobile_ids = fields.One2many('project.mobilenumbers', 'partner_id', string='mobile numbers')
+      order_ids =  fields.One2many('pos.order','delivery_man_id',select=True, string='Latest Orders', limit=5)
+      status=fields.Selection([('a','Avaliable'),('b','Busy')],readonly=True)
+              
