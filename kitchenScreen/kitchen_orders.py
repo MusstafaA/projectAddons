eea4132b96be450f02e68eef04193de4e9e86d8e
Mustafa <mustafa.cu13@yahoo.com>
2016-06-06 17:41:53 +0200

    adding changing stage button in tree view

--- kitchenScreen/kitchen_orders.py
+++ kitchenScreen/kitchen_orders.py
@@ -1,9 +1,22 @@
-from openerp import models, fields
+from openerp import models, fields,api
 
 
 class KitchenOrders(models.Model):
     _inherit = 'pos.order'
     _name = "pos.order"
+
+    @api.one
+    def change_stage(self):
+        x = self.stage
+        if x == 'kitchen' :
+            self.stage = 'Delivery'
+            return self.stage
+        elif x == 'Delivery' :
+            self.stage = 'Delivered'
+        else:
+            self.stage = 'kitchen'
+
+
 
     stage = fields.Selection([('kitchen', 'In Kitchen'),('Delivery', 'Out for Delivery'),('Delivered', 'Delivered')],'Current Stage', default= 'kitchen' ,readonly=False, copy=False)
 
