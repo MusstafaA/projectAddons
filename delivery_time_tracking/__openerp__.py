1d4cbd0894388f91f11006a02f6981f17d5b8d68
mahahamza <maha_hamza2010@yahoo.com>
2016-06-08 22:45:59 +0200

    Manufacturing and delivery time tracking

---
+++ delivery_time_tracking/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name':'Delivery Time Tracking',
+    'version':'1.1',
+    'author':'ITI'
+    ,'category':'point of sale',
+    'website':'iti.odoo.com',
+    'depends': ['DeliveryMan'],
+    'data': ['delivery.xml'],
+    'installable':True
+
+}
+
+
+
