51219ebaf32d15fd44b92b1188861a42ea2dd1c1
Mostafa Abd El Fattah <mostafa.abdelfattah3@gmail.com>
2016-06-09 09:53:56 +0200

    Add a controller to the session with a new route

--- session_ui/__openerp__.py
+++ session_ui/__openerp__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'author': 'OSAD_Odoo_Team',
     'category': 'other',
-    'depends':['base','point_of_sale'],
+    'depends':['base','point_of_sale','pos_restaurant','website','website_blog'],
     'website': 'http://iti.gov.eg',
     'description':"""
 POS_Session_Modifications
@@ -11,5 +11,9 @@ POS_Session_Modifications
     """,# here we are importing all the views that we made before
     'data': [
         'views/pos_dashboard_view.xml',
+        'views/start_up.xml',
             ],
+    'qweb':[
+        'themes/assets.xml',
+          ],
 }
--- session_ui/controllers/main.py
+++ session_ui/controllers/main.py
@@ -4,6 +4,8 @@ import werkzeug.utils
 
 from openerp import http
 from openerp.http import request
+from openerp import SUPERUSER_ID
+from openerp.addons.web import http
 
 # Print debug message
 _logger = logging.getLogger(__name__)
@@ -11,7 +13,7 @@ _logger = logging.getLogger(__name__)
 
 class SessionController(http.Controller):
 
-    @http.route('/pos/startup', type='http', auth='user')
+    @http.route('/pos/startup', type='http', auth='user',  website=True)
     def startup(self, debug=False, **k):
-        return "<h1>This is a test</h1>"    
-
+		return request.website.render("website.services",{'name':"Mostafa"})	
+		#return "<h1>This is a test</h1>"
