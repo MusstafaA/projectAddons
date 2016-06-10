dbee8616d7c4f0ac62d4585dcf343bab140a0815
mohammed <mada247@gmail.com>
2016-06-10 10:57:34 +0200

    assign delivery man to order

--- tyaar/tyaar.py
+++ tyaar/tyaar.py
@@ -1,16 +1,12 @@
 from openerp import fields,models,api
 
 
-# class delivery(models.Model):
-#     _name = "iti.delivery"
-#
-#     name=fields.Char()
 
 
 class Order(models.Model):
     _inherit="pos.order"
 
-#     delivery_id=fields.Many2one("hr.employee",string="Delivery")
+    delivery_id=fields.Many2one("hr.employee",string="Delivery")
 
 
 
